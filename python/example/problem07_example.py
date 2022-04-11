a2808ad72a0ed512a0fa247e249a75b99afa313b
matenduel <matenduel@gmail.com>
2022-04-11 22:29:38 +0900

    Add: make problem07.py

--- python/example/problem07_example.py
+++ python/example/problem07_example.py
@@ -16,13 +16,21 @@ import pandas as pd
 
 def main() -> dict:
     result = {}
-    file_path = "./files/product_list_with_imgs.parquet"
+    file_path = "../files/product_list_with_imgs.parquet"
 
     # Step 1: Read parquet data
+    product_datas = pd.read_parquet(file_path)
 
     # Step 2: Split the img field to list
-    # Step 3: extract the image format
-    # Step 4: count the image format
+    for images in product_datas["img"]:
+        for image in images.split("|"):
+            # Step 3: extract the image format
+            path_and_file_name, ext = image.rsplit(".", 1)
+            # Step 4: count the image format
+            if ext not in result:
+                result[ext] = 1
+            else:
+                result[ext] += 1
 
     return result
 
